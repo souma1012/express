7d385ff4c7d8200b1687fc22cf5344f9243dd84e
souma1012 <2201105047ew@cyber-u.ac.jp>
2026-07-18 17:20:46 +0900

    add env

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
 var catRouter = require('./routes/cat');
 var qrRouter = require('./routes/qr');
+var notes_from_bRouter = require('./routes/notes_from_b');
 
 var app = express();
 
@@ -29,6 +30,8 @@ app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
 app.use('/cat', catRouter);
 app.use('/qr', qrRouter);
+app.use('/notes_from_b', notes_from_bRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
